0d0fdb8c2975d1085c4e5dd4afe4b327cbf296b1
Stas Myronenko <stasmyronenko96@gmail.com>
2023-05-24 12:52:36 +0300

    fix: turn off logs

--- config/db.py
+++ config/db.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "mssql+pyodbc://SA:Admin123!@0.0.0.0:1433/AccountingMoneyWithORM?driver=ODBC+Driver+17+for+SQL+Server"
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False)
 
 Session = sessionmaker(engine)
